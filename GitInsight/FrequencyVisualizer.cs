8e0a9cc8b67dfcac13770dd5f7b2bcf20e9f9a29
Anders Arvesen <aarv@itu.dk>
2022-10-30 17:25:47 +0100

    no changes

--- GitInsight/FrequencyVisualizer.cs
+++ GitInsight/FrequencyVisualizer.cs
@@ -4,12 +4,9 @@ public class FrequencyVisualizer : IVisualizer{
     List<FrequencyDTO> dtos;
     public FrequencyVisualizer(List<FrequencyDTO> dtos){
         this.dtos = dtos;
-    }
-
-     public void visualize(){
+    }     public void visualize(){
         foreach (FrequencyDTO dto in dtos){
             Console.WriteLine(dto.frequency + " " + dto.dateTime);
         }
     }
-
 }
