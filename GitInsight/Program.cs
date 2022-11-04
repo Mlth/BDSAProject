3e4b3bf7fede45d608920acf76c4607462670f05
Martin <mwha@itu.dk>
2022-11-04 12:26:04 +0100

    Added global usings for GitInsight

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,11 +1,18 @@
 ﻿namespace GitInsight;
-using GitInsight.Entities;
-using LibGit2Sharp;
+
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        RepositoryContextFactory factory = new RepositoryContextFactory();
+        RepositoryContext context = factory.CreateDbContext(args);
+        Console.WriteLine($"Database path: {context.DbPath}.");
+        context.CommitData.Add(new RepositoryCommitData{Id = 1, author = "MCAndersYo", frequencies = new List<Core.FrequencyDTO>{}, dateTime = new DateTime(2022, 11, 3)});
+        var commitData = context.CommitData
+            .OrderBy(c => c.Id)
+            .First();
+        Console.WriteLine(commitData.author);
         var repository = new Repository(args[0]);
         var command = Factory.getCommand(args[1]);
         command.execute(repository);
---
+++ GitInsight/Usings.cs
@@ -0,0 +1,5 @@
+global using GitInsight.Entities;
+global using LibGit2Sharp;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Design;
+global using System.Linq;
