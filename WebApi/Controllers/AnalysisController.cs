f0ffd695d98bf47aa483b0e0ef7e90fc5a7b65e7
luel <luel@itu.dk>
2022-11-15 11:25:16 +0100

    refactored analysiscontroller to webprogram class

--- WebApi/Controllers/AnalysisController.cs
+++ WebApi/Controllers/AnalysisController.cs
@@ -16,49 +16,11 @@ public class AnalysisController : ControllerBase
     public string Get(string github_user, string repository_name, string command)
     {
         var repositoryPath = "https://github.com/" + github_user + "/" + repository_name + ".git";
-
         var path = Directory.GetCurrentDirectory();
-        
-        var newDir = path + "/" + repository_name;
-
-        string existingRepo = path + "/" + repository_name;
-        if(Directory.GetDirectories(path, repository_name).Length < 1){
-            cloneRepository(repositoryPath, newDir);
-        } else {
-            pullRepository(existingRepo);
-        }
 
         WebProgram webProgram = new WebProgram();
-        var jsonString = webProgram.GetAnalysisJsonString(newDir, command);
-        
-        deleteDirectory(newDir);
+        var jsonString = webProgram.GetAnalysisJsonString(repositoryPath, repository_name, command);
 
         return jsonString;
-    }
-
-    public static void cloneRepository(string repositoryPath, string newDir){
-        Repository.Clone(repositoryPath, newDir);
-    }
-
-    public static void pullRepository(string repositoryPath){
-        var repository = new Repository(repositoryPath);
-        var signature = new Signature("Lukas", "luel@itu.dk", DateTime.Now);
-        Commands.Pull(repository, signature, null);
-    }
-
-    public static void deleteDirectory(string path)
-    {
-        foreach (var subdirectory in Directory.EnumerateDirectories(path))
-        {
-            deleteDirectory(subdirectory);
-        }
-        foreach (var fileName in Directory.EnumerateFiles(path))
-        {
-            var fileInfo = new FileInfo(fileName);
-            fileInfo.Attributes = FileAttributes.Normal;
-            fileInfo.Delete();
-        }
-        var dir = new DirectoryInfo(path);
-        dir.Delete(true);
     }
 }
