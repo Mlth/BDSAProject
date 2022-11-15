6bf4e99c45ea6b38ca1411fb39530381c8a26e60
Anders Arvesen <aarv@itu.dk>
2022-11-15 13:26:51 +0100

    changed dbcommit

--- GitInsight.Entities/DBCommit.cs
+++ GitInsight.Entities/DBCommit.cs
@@ -1,7 +1,7 @@
 namespace GitInsight.Entities;
 public record DBCommit(){
 
-    public int Id {get; set;}
+    //public int Id {get; set;}
     public string author {get; set;}
 
     public DateTime date {get; set;}
