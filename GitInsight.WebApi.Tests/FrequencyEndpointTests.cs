704a03d4b8d6b42bddbb0c2232023b99eadfd8c4
mlth <mlth@itu.dk>
2022-11-20 16:33:33 +0100

    changed date format to make github actions happy

--- GitInsight.WebApi.Tests/FrequencyEndpointTests.cs
+++ GitInsight.WebApi.Tests/FrequencyEndpointTests.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using GitInsight.Entities.DTOS;
+using System.Globalization;
 
 namespace GitInsight.WebApi.Tests;
 
@@ -16,15 +17,14 @@ public class FrequencyEndpointTest : IClassFixture<CustomWebApplicationFactory> 
     public async Task Get()
     {
         var frequencies = await client.GetFromJsonAsync<FrequencyDTO[]>("analysis/TestUser/TestRepo/frequency");
-
-        var commitDateTime1 = new DateTime(2022, 10, 10);
-        var commitDateTime2 = new DateTime(2022, 10, 11);
+        
+        DateTimeFormatInfo dtfi = CultureInfo.CreateSpecificCulture("da-DK").DateTimeFormat;
 
         //Assert.Equal<Object>($"2 {commitDateTime}", frequencyArray[0]);
         Assert.Equal(2, frequencies[0].frequency);
-        Assert.Equal("10-10-2022", frequencies[0].date.ToShortDateString());
+        Assert.Equal("10-10-2022", frequencies[0].date.ToString("d", dtfi));
         Assert.Equal(1, frequencies[1].frequency);
-        Assert.Equal("11-10-2022", frequencies[1].date.ToShortDateString());
+        Assert.Equal("11-10-2022", frequencies[1].date.ToString("d", dtfi));
     }
 
 }
