b07541a1c64145b2018d2e4942aec9cb9209fbc1
mlth <mlth@itu.dk>
2022-11-21 22:00:45 +0100

    small changes to WebApiTests

--- GitInsight.WebApi.Tests/TestPriorityAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace GitInsight.WebApi.Tests;
-
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public class TestPriorityAttribute : Attribute
-{
-    public int Priority { get; init; }
-
-    public TestPriorityAttribute(int priority) => Priority = priority;
-}
--- GitInsight.WebApi/TestGitFetcher.cs
+++ GitInsight.WebApi/TestGitFetcher.cs
@@ -6,7 +6,6 @@ public class TestGitFetcher : IGitFetcher {
     public void cloneRepository(string repositoryPath, string newDir){
         var repo = new Repository(Repository.Init(newDir));
 
-        //Tilføjt nogle commits
         var commitDateTime1 = new DateTime(2022, 10, 01);
         var author1 = new Signature("mlth", "mlth@itu.dk", commitDateTime1);
         var commitDateTime2 = new DateTime(2022, 10, 01);
@@ -14,7 +13,6 @@ public class TestGitFetcher : IGitFetcher {
         var commitDateTime3 = new DateTime(2022, 10, 02);
         var author3 = new Signature("aarv", "aarv@itu.dk", commitDateTime3);
 
-        //Act
         repo.Commit("First commit", author1, author1, new CommitOptions(){ AllowEmptyCommit = true });
         repo.Commit("Second commit", author2, author2, new CommitOptions(){ AllowEmptyCommit = true });
         repo.Commit("Third commit", author3, author3, new CommitOptions(){ AllowEmptyCommit = true });
