be83c40b9d870b1edf82efa7bf941a796dd40a9c
luel <luel@itu.dk>
2022-11-22 12:50:22 +0100

    fixed github credentials

--- GitInsight/ForkCommand.cs
+++ GitInsight/ForkCommand.cs
@@ -5,19 +5,19 @@ using System.Text.Json;
 
 public class ForkCommand {
 
+    GitHubClient client;
+    Octokit.Credentials credentials;
 
     public async Task<IEnumerable<ForkDTO>> analyzeForks(string githubApiKey, string username, string repoName){
         var productInformation = new ProductHeaderValue("luel");
-        var credentials = new Octokit.Credentials("");
         if(githubApiKey == null || githubApiKey.Length == 0){
-            credentials = new Octokit.Credentials("0be8860aee462442");
+            client = new GitHubClient(productInformation);
         } else {
             credentials = new Octokit.Credentials(githubApiKey);
+            client = new GitHubClient(productInformation) { Credentials = credentials };
         }
-        
-        var client = new GitHubClient(productInformation) { Credentials = credentials };
 
-        IReadOnlyList<Octokit.Repository> allForks = await client.Repository.Forks.GetAll("Mlth", "BDSAProject");
+        IReadOnlyList<Octokit.Repository> allForks = await client.Repository.Forks.GetAll(username, repoName);
         return from f in allForks select new ForkDTO{url = f.Url};
     }
 
