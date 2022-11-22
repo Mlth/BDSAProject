14e73b335e276617fb2967e58fd7cff1d30de3fa
Anton Vitus Sohl Andersen <anton.sohl@gmail.com>
2022-11-22 13:14:19 +0100

    First test

--- BlazorApp.Tests/AnalysisTests.cs
+++ BlazorApp.Tests/AnalysisTests.cs
@@ -1,6 +1,23 @@
+using Radzen.Blazor;
+
 namespace BlazorApp.Tests;
+
 
 public class AnalysisTests
 {
-    
+    [Fact]
+    public void TextBox_Renders_ValueParameter()
+    {
+        using var ctx = new TestContext();
+
+        var component = ctx.RenderComponent<Analysis>();
+
+        var radzenCom =  component.FindComponent<RadzenTextBox>();
+
+        var value = "Test";
+
+        radzenCom.SetParametersAndRender(parameters => parameters.Add(p => p.Value, value));
+
+        Assert.Contains(@$"value=""{value}""", component.Markup);
+    }
 }
